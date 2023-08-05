eed0d58b9fa806bbffa6a5f548ab9463da3f70cb
thatonecodes <almoussalymaher@gmail.com>
2023-08-05 18:36:45 -0400

    fix for help, template, peacetime

--- cogs/basic/template.py
+++ cogs/basic/template.py
@@ -6,7 +6,7 @@ class template(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @app_commands.command(name="template", description="description", category="basic")
+    @app_commands.command(name="template", description="description")
     async def template(self,interaction: discord.Interaction):
         await interaction.response.send_message("command")
 
